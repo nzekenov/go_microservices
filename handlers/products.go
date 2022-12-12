593e1d6eecf5a33650ac5a76019746febba4d602
Nurassyl Zekenov <nurassyl@snoonu.com>
2022-12-12 21:48:04 +0300

    Second commit: Implemented POST & PUT

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"microservices/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -19,15 +21,75 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
 		p.getProducts(rw, h)
 		return
 	}
-	// handle update
 
+	if h.Method == http.MethodPost {
+		p.addProduct(rw, h)
+		return
+	}
+
+	if h.Method == http.MethodPut {
+		p.l.Println("PUT")
+		// expect the id in the path
+		reg := regexp.MustCompile("/([0-9]+)")
+		g := reg.FindAllStringSubmatch(h.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+		p.updateProducts(id, rw, h)
+		return
+	}
+	// handle update
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+	p.l.Println("Handle GET Product")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall JSON", http.StatusBadRequest)
+	}
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall JSON", http.StatusBadRequest)
+	}
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+
 }
